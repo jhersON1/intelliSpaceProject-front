bec11eadc42c3c659f40601b264df492a5d6c7d0
jherson <rodriguezpaul98@gmail.com>
2025-04-09 23:47:53 -0400

    dashboard improved, navbar mobile improved

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,34 +1,45 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
   isNavbarHidden = false;
   private lastScrollTop = 0;
-  private scrollThreshold = 5; // Minimum scroll amount to trigger hide/show
 
-  constructor() { }
+  isMobileMenuOpen = false;
+  isMobileSearchOpen = false;
 
-  ngOnInit(): void {
-  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
-    // Determine scroll direction
     if (currentScrollTop > this.lastScrollTop && currentScrollTop > 50) {
-      // Scrolling down & not at the top
       this.isNavbarHidden = true;
     } else {
-      // Scrolling up or at the top
       this.isNavbarHidden = false;
     }
     
-    // Update scroll position
     this.lastScrollTop = currentScrollTop;
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    
+    if (this.isMobileMenuOpen) {
+      this.isMobileSearchOpen = false;
+    }
+  }
+
+  toggleMobileSearch(): void {
+    this.isMobileSearchOpen = !this.isMobileSearchOpen;
+    
+    if (this.isMobileSearchOpen) {
+      this.isMobileMenuOpen = false;
+    }
   }
 }
